8d8dcd16167e5d277f8856b5440b13874c4341d0
James Yarbrough <jjarbrough@gmail.com>
2023-10-06 14:46:25 -0500

    finished the exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2004;
+            Car momCar = new Car();
+            momCar.Make = "Volvo";
+            momCar.Model = "S-60";
+            momCar.Year = 2013;
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
         }
     }
 }
